d724141b22078a53ba0f57ac6a9280ed3807e2a4
macabeus <bruno.macabeus@gmail.com>
2019-10-25 13:45:42 +0100

    server: add cors

--- app_list_server/src/index.js
+++ app_list_server/src/index.js
@@ -1,8 +1,10 @@
 const Koa = require('koa')
+const cors = require('@koa/cors')
 const router = require('./routes')
 
 const app = new Koa()
 
 app
+  .use(cors())
   .use(router.routes())
   .listen(3000)
